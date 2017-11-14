f1c8ce487983e8eef59a625d267bdaf5b056543f
Uemerson <Uemerson@DESKTOP-AN6V6BA>
2017-11-13 23:09:02 -0200

    menu + outros forms

--- src/controler/Main.java
+++ src/controler/Main.java
@@ -1,7 +1,27 @@
 package controler;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import view.frmMenu;
+
 public class Main {
 	public static void main(String Args[]) {
-		System.out.println("Projeto iniciado");
+		
+		//Muda o look and feels padrão do java
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+		//Chama o menu principal
+		new frmMenu().setVisible(true);
 	}
 }
